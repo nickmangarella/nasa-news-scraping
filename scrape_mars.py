eb804db6dea85bc41b3cab921530d1087510510a
nickm <nickmangarella89@gmail.com>
2021-02-09 17:42:51 -0500

    removed html table file

--- scrape_mars.py
+++ scrape_mars.py
@@ -60,7 +60,7 @@ def scrape():
     df.rename(columns={0: '', 1: 'Mars'}, inplace=True)
 
     # Convert the DataFrame to an HTML table string
-    mars_table = print(df.to_html())
+    mars_table = df.to_html()
 
     # Website to open in chrome
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
